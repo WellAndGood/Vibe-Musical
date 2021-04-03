e371a89651628bfb57390f4585d0e313cb98f0c4
Dipti2021 <77368913+Dipti2021@users.noreply.github.com>
2021-04-03 11:39:31 -0400

    Add files via upload

---
+++ assets/script/magic.js
@@ -0,0 +1,23 @@
+var pianoplay = document.getElementById("woodplay");
+var drumplay = document.getElementById("keyplay");
+var guitarplay = document.getElementById("stringplay");
+var sing = document.getElementById("sing");
+
+document.getElementById("woodplay").onclick = function () {
+    location.href = "https://surikov.github.io/webaudiofont/examples/flute.html";
+};
+
+document.getElementById("keyplay").onclick = function () {
+    location.href = "https://www.musictheory.net/piano";
+};
+
+document.getElementById("stringplay").onclick = function () {
+    location.href = "https://musiclab.chromeexperiments.com/Strings/";
+};
+document.getElementById("sing").onclick = function () {
+    location.href = "file:///C:/Users/razda/OneDrive/Desktop/project1/try3/guitar.html";
+
+}
+
+
+    
