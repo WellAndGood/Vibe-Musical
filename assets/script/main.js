6656b5af00ca65c8b0ce7cdfe41f73cdf4c0092a
github <calbest31@outlook.com>
2021-04-06 21:45:37 -0400

    popup window added into tut section

--- assets/script/main.js
+++ assets/script/main.js
@@ -9,3 +9,15 @@ $(document).ready(function() {
   
     });
   });
+
+  document.addEventListener('DOMContentLoaded', () => {
+    (document.querySelectorAll('.notification .delete') || []).forEach(($delete) => {
+      const $notification = $delete.parentNode;
+  
+      $delete.addEventListener('click', () => {
+        $notification.parentNode.removeChild($notification);
+      });
+    });
+  });
+  
+  
